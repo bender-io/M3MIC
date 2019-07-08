7716eed2c2d609843b5435b0149a3065e2a94cd3
Brian Hersh <bri.d.hersh@gmail.com>
2019-07-08 16:32:58 -0600

    adds icons, storyboards, and PostVC

---
+++ M3MIC/Helpers/MemicUI/MemicTV.swift
@@ -0,0 +1,23 @@
+//
+//  MemicTV.swift
+//  M3MIC
+//
+//  Created by Brian Hersh on 7/8/19.
+//  Copyright © 2019 Brian Daniel. All rights reserved.
+//
+
+import UIKit
+
+class MemicTV: UITextView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupUI()
+    }
+    
+    func setupUI() {
+        self.backgroundColor = .white10
+        self.cornerRadios()
+    }
+    
+}
--- M3MIC/View Controllers/Navigation/PostVC.swift
+++ M3MIC/View Controllers/Navigation/PostVC.swift
@@ -10,8 +10,23 @@ import UIKit
 
 class PostVC: UIViewController {
 
+    @IBOutlet weak var postTV: MemicTV!
+    @IBOutlet weak var publicSwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         viewSetup()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        postTV.becomeFirstResponder()
+    }
+    
+    @IBAction func cancelButtonTapped(_ sender: Any) {
+        tabBarController?.selectedIndex = 0
+    }
+    
+    @IBAction func publicSwitchTapped(_ sender: Any) {
     }
 }
