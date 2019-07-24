c1ac4c6a50ded6f0e89aad1184114b348ed0daa1
Brian Hersh <bri.d.hersh@gmail.com>
2019-07-24 04:49:16 -0600

    minor fixes and TODOs

---
+++ M3MIC/Resources/ToDo.swift
@@ -0,0 +1,41 @@
+//
+//  ToDo.swift
+//  M3MIC
+//
+//  Created by Brian Hersh on 7/23/19.
+//  Copyright © 2019 Brian Daniel. All rights reserved.
+//
+
+/*
+ 
+TODO: - HIGH PRIORITY
+    1. Hide posts where blockedUID == post.userUID
+    2. Fix imageURL array to save and fetch properly
+    3. Add profilePicture to FSStorage and fetch from user.profilePicture (singleton throughout?)
+ 
+TODO: - MEDIUM PRIORITY
+    1. Filter posts by timestamp
+    2. Replace images with gifs
+    3. Auto-clear  creatPostTextField once typing
+    4. Ability to delete your reply
+    5. Ability to delete your post
+    6. Add username to reply
+ 
+TODO: - LOW PRIORITY
+    1. Change status bar color to white on certain VC's
+    2. Change FriendUIDs / BlockedUIDs to usernames on friendVC
+ 
+TODO: - WISH LIST
+    1. Add like and rank to Reply
+    2. Feed toggle for public / private posts
+    3. Gif chat groups with friends
+    4. SplashScreen animation
+    5. Local Notifications
+    6. Constraints for XR, XS, and 8
+    7. Import friends from contacts / facebook
+ 
+*/
+
+
+
+
